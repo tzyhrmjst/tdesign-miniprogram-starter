c78401fd6216c99ada07d0594c5988d68e86ad50
Charlie <charlie@CharliedeMac-mini.local>
2026-06-03 23:52:26 +0800

    Reduce WeChat mini program package size

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 // app.js
-import config from './config';
-import Mock from './mock/index';
 import createBus from './utils/eventBus';
-
-if (config.isMock) {
-  Mock();
-}
 
 App({
   onLaunch() {
